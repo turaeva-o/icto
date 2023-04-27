f38cc96660779b17ef79df8bd84a5691639222f2
turaeva-o <124877522+turaeva-o@users.noreply.github.com>
2023-04-27 21:41:50 +0300

    Add files via upload

---
+++ week08/registerevents.js
@@ -0,0 +1,11 @@
+﻿AFRAME.registerComponent('registerevents', {
+	init: function () {
+		var marker = this.el;
+		marker.addEventListener('markerFound', function() {
+			console.log('markerFound'); 
+		});
+		marker.addEventListener('markerLost', function() {
+			console.log('markerLost'); 
+		});
+	}
+});
