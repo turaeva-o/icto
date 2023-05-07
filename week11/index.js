c9f1f49fea3cc6a141136a8bf38d9402f6bb1cd7
turaeva-o <124877522+turaeva-o@users.noreply.github.com>
2023-05-07 19:18:58 +0300

    Update index.js

--- week11/index.js
+++ week11/index.js
@@ -124,7 +124,8 @@ function main() {
         arjs.add(new THREE.Mesh(geom, material2), longitude, latitude - 0.001); // slightly south
         arjs.add(new THREE.Mesh(geom, material3), longitude - 0.001, latitude); // slightly west
         arjs.add(new THREE.Mesh(geom, material4), longitude + 0.001, latitude); // slightly east
-	    arjs.add(cat.scene, longitude, latitude + 0.001);
+	    //arjs.add(cat.scene, longitude, latitude + 0.001);
+	    cat.scene (longitude, latitude + 0.001);
     }
 
     requestAnimationFrame(render);
